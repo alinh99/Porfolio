3bff0ee3c82369148731db8d8c4fb74e8a508d03
alinh99 <alinh1803@gmail.com>
2024-02-19 00:54:11 +0700

    chores: Remove unnecessary files

--- database.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Skills(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    proficiency = db.Column(db.Integer)
