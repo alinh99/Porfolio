d58232df05116a3514f48d7d08c503ad20722a0d
alinh99 <alinh1803@gmail.com>
2024-02-19 11:55:55 +0700

    fix(send_email): Receive email in contact form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from admin import skills, general_information, experience, projects
 from email.mime.text import MIMEText
 from smtplib import SMTP_SSL as SMTP
 import os
+from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///portfolio.db"
@@ -24,21 +25,20 @@ def homepage():
         email = request.form.get("email")
         subject = request.form.get("subject")
         message = request.form.get("message")
-        try:
-            mail=SMTP(SMTP_SERVER, PORT)
-            message = f"Name: {name}" + "\n" + f"Email: {email}" + "\n" + f"Message: {message}"
-            msg = MIMEText(message, "plain")
-            msg["Subject"] = subject
-            msg['From'] = email
+        
+        mail=SMTP(host=SMTP_SERVER, port=PORT)
+        
+        mail.login(general_information["email"], PASSWORD)
+        
+        contents = f"Name: {name}" + "\n" + f"Email: {email}" + "\n" + f"Message: {message}"
 
-            mail.set_debuglevel(False)
-            mail.starttls()
-            mail.login(general_information["email"], PASSWORD)
-            
-            mail.sendmail(msg['From'],general_information["email"], msg.as_string())
-            return redirect(url_for("thank_you"))
-        except Exception as e:
-            return str(e)
+        msg = MIMEText(contents)
+        msg["Subject"] = subject
+        
+        mail.sendmail(email, RECEPIENT_EMAIL, msg=msg.as_string())
+        mail.close()
+        return redirect(url_for("thank_you"))
+
     return render_template('index.html', skill_length=skills.items(), information=general_information, exp=experience, projects=projects)
 
 @app.route('/portfolio-details/<int:portfolio_id>')
@@ -49,31 +49,6 @@ def portfolio_details(portfolio_id):
 def html_page(page_name):
     return render_template(page_name)
 
-# def write_to_file(data):
-#     with open('database.txt', mode='a') as database:
-#         email = data['email']
-#         subject = data['subject']
-#         message = data['message']
-#         file = database.write(f'\n{email},{subject},{message}')
-
-
-def write_to_csv(data):
-    with open('database.csv', 'a', newline='') as csvfile:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        writer = csv.writer(csvfile)
-        writer.writerow([email, subject, message])
-
-
-@app.route('/submit_form', methods=['GET', 'POST'])
-def submit_form():
-    if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_csv(data)
-        return redirect('/thankyou.html')
-    else:
-        return 'Something went wrong!'
 
 if __name__ == "__main__":
     app.run(debug=True)
